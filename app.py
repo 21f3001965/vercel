7a4b6a2048dc69a799b860949d8b5f81ea89bc2c
21f3001965 <21f3001965@ds.study.iitm.ac.in>
2025-01-26 11:00:10 +0530

    template added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import json
 
 app = Flask(__name__)
@@ -8,7 +8,6 @@ with open('tds.json','r') as f:
 
 @app.route('/')
 def home():
-    return {"hello"}
-
+    return render_template('home.html')
 if __name__ == "__main__":
     app.run()
