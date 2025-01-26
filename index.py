85471bc59c4a639ffc8ee2adbb9b0e1acf122e33
21f3001965 <21f3001965@ds.study.iitm.ac.in>
2025-01-26 14:17:41 +0530

    small

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify("hello")
+    return "Hello, Flask!"
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
